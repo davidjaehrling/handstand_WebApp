90438c3cc284317b7494ad682cb1a07bc9c8f5ae
davidjaehrling <106091711+davidjaehrling@users.noreply.github.com>
2023-02-20 12:05:01 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -166,7 +166,7 @@ function getDistance(keypointsOld, keypointsNew) {
     navigator.mediaDevices
       .getUserMedia({ 
         audio: false,
-        video: { facingMode: {exact: "user"}, width: 240, height: 426 ,frameRate: {ideal:30}}
+        video: { facingMode: {exact: "user"}, width: 480, height: 640 ,frameRate: {ideal:30}}
       }).then(stream => {
         let video = videoRef.current;
         video.setAttribute('autoplay', '');
@@ -205,8 +205,8 @@ function getDistance(keypointsOld, keypointsNew) {
             right: 0,
             textAlign: "center",
             zindex: 9,
-            width: 240,//480,
-            height: 426,//640,
+            width: 480,
+            height: 640,
           }}
         />
 
@@ -220,8 +220,8 @@ function getDistance(keypointsOld, keypointsNew) {
             right: 0,
             textAlign: "center",
             zindex: 9,
-            width: 240,//480,
-            height: 426,//640,
+            width: 480,
+            height: 640,
           }}
         />
 
